b49c0b389b24de450a62a0b423515059c3a6ed32
Jferpon <jferpon089@g.educaand.es>
2026-01-07 14:13:00 +0100

    Commit para el mini reto del hito1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 // Servir un archivo HTML simple
 app.get('/', (req, res) => {
-  res.send('<h1>Hola, Mundo desde Node.js!</h1>');
+  res.send('<h1>Hola,este es el primer commit para el reto!</h1>');
 });
 
 app.listen(port, () => {
